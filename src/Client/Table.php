750ede9e4b7bd66f81cfb0b6b6024c32e462c7c6
William Olding <william.olding@cityfibre.com>
2022-11-18 14:07:32 +0000

    Added get Display values method

--- src/Client/Table.php
+++ src/Client/Table.php
@@ -93,5 +93,13 @@ class Table
         return json_decode($response->getBody());
     }
 
+    public function whereWithDisplayValues($table, $field, $value, $operator = '=', $limit = 1)
+    {
+        $response = $this->client->get('/api/now/table/'  . $table .
+            '?sysparm_query=' . $field . $operator . $value .
+            '&sysparm_display_value=true' .
+            '&sysparm_limit=' . $limit , ['headers' => $this->headers]);
+        return json_decode($response->getBody());
+
 
 }
