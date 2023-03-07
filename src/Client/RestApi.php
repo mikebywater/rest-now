2d2d31c7b2f821f8473f57da62b4d6e1ff1e0af5
oleshchss <127214429+oleshchss@users.noreply.github.com>
2023-03-07 16:05:43 +0200

    add restApiPost method

--- src/Client/RestApi.php
+++ src/Client/RestApi.php
@@ -5,22 +5,39 @@ namespace Now\Client;
 class RestApi extends Table
 {
 
-    public function restApiGet($nameSpace,$apiName, $endPointName, $queryParameters)
+    public function restApiGet($nameSpace, $apiName, $endPointName, $queryParameters)
     {
         $queryString = $this->getQueryString($queryParameters);
-        $response = $this->client->get('/api/' .
-            $nameSpace .  '/' .
+        $response = $this->client->get(
+            '/api/' .
+            $nameSpace . '/' .
             $apiName . '/' .
-            $endPointName . '?' .
+            $endPointName .
             $queryString,
-            ['headers' => $this->getHeaders()]);
+            ['headers' => $this->getHeaders()]
+        );
+
+        return json_decode($response->getBody());
+    }
+
+    public function restApiPost($nameSpace, $apiName, $endPointName, $queryParameters = [], $data = [])
+    {
+        $queryString = !empty($queryParameters) ? $this->getQueryString($queryParameters) : '';
+        $response = $this->client->post(
+            '/api/' .
+            $nameSpace . '/' .
+            $apiName . '/' .
+            $endPointName .
+            $queryString,
+            ['headers' => $this->getHeaders(), 'form_params' => $data]
+        );
 
         return json_decode($response->getBody());
     }
 
     public function getQueryString($queryParameters)
     {
-        $queryString = '';
+        $queryString = '?';
         $operator = '';
         foreach ($queryParameters as $queryParameter => $queryValue) {
             $queryString = $queryString . $operator . $queryParameter . '=' . $queryValue;
